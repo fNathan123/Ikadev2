0d060d425391b96b678ee43ef551a90b4b0ab405
fNathan123 <mrfeb011@gmail.com>
2025-07-11 10:42:21 +0700

    Fix : truck implementation errors

--- game/truck/TruckModel.gd
+++ game/truck/TruckModel.gd
@@ -1,18 +1,18 @@
 class_name TruckModel
 
 var package_id : Array[int] :
-    get: return package_id;
+	get: return package_id;
 
 func init_async() :
-    if(GameService.is_ready):
-        GameService.instance._init();
-    package_id = GameService.instance.data.package_pool;
-    package_id.shuffle();
+	if(!GameService.is_ready):
+		await GameService.instance.wait_service();
+	package_id = GameService.instance.data.package_pool;
+	package_id.shuffle();
 
 var last_package_idx = 0;
 func get_package()->int:
-    if last_package_idx >= package_id.size():
-        return -1;
-    var res = package_id[last_package_idx];
-    last_package_idx += 1;
-    return res;
+	if last_package_idx >= package_id.size():
+		return -1;
+	var res = package_id[last_package_idx];
+	last_package_idx += 1;
+	return res;
